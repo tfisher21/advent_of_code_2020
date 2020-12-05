4d4cb82da3727712a718f1b47ec73a67465bf3f4
repl.it user <replituser@example.com>
2020-12-05 18:27:06 +0000

    Refactor part_one for use in part_two

--- lib/day_three.rb
+++ lib/day_three.rb
@@ -12,20 +12,30 @@ class DayThree
       end
     end
 
-    def part_one
-      r_index = 0
+    def part_one(down: 1, right: 3)
+      h_index = 0
       v_index = 0
       tree_count = 0
       line_length = input_data[0].length
 
       while(v_index < input_data.length - 1)
-        v_index += 1
-        r_index += 3
+        v_index += down
+        h_index += right
 
-        r_index = r_index >= line_length ? r_index - line_length : r_index
-        tree_count += 1 if input_data[v_index][r_index] == "#"
+        h_index = h_index >= line_length ? h_index - line_length : h_index
+        tree_count += 1 if input_data[v_index][h_index] == "#"
       end
       tree_count
+    end
+
+    def part_two
+      [
+        part_one(right:1, down: 1),
+        part_one(right:3, down: 1),
+        part_one(right:5, down: 1),
+        part_one(right:7, down: 1),
+        part_one(right:1, down: 2),
+      ].reduce(:*)
     end
   end
 end
