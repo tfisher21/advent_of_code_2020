f48ca3cea2030b41e8f86fc31a3697c76ac57811
Tyler Fisher <tyler.r.fisher@icloud.com>
2020-12-06 06:25:45 +0000

    Day Five Part Two

--- lib/day_five.rb
+++ lib/day_five.rb
@@ -7,31 +7,48 @@ class DayFive
     end
 
     def part_one
-      highest_seat_id(input_data)
+      seat_ids(input_data).last
     end
 
-    def highest_seat_id(tickets)
-      seat_ids = []
-      tickets.each do |ticket|
-        row = (0..127)
-        seat = (0..7)
-        
-        ticket.each_char do |c|
-          if c.upcase == "F"
-            row = Range.new(row.first, row.last - row.size / 2)
-          elsif c.upcase == "B"
-            row = Range.new(row.first + row.size / 2, row.last)
-          elsif c.upcase == "L"
-            seat = Range.new(seat.first, seat.last - seat.size / 2)
-          elsif c.upcase == "R"
-            seat = Range.new(seat.first + seat.size / 2, seat.last)
-          else
-            raise RuntimeError, "invalid input_data #{ticket}:#{c}"
-          end
+    def part_two
+      seat_ids = seat_ids(input_data)
+      personal_seat_id = -1
+
+      seat_ids.each_with_index do |item, index|
+        next if index == 0 || index == seat_ids.length - 1
+        unless item + 1 == seat_ids[index + 1]
+          personal_seat_id = item + 1
         end
-        seat_ids << (row.last * 8) + seat.last
+
+        unless item - 1 == seat_ids[index - 1]
+          personal_seat_id = item - 1
+        end
       end
-      seat_ids.sort.last
+      personal_seat_id
+    end
+
+    def seat_ids(tickets)
+      tickets.each_with_object([]) { |item, array| array << seat_id(item) }.sort
+    end
+
+    def seat_id(ticket)
+      row = (0..127)
+      seat = (0..7)
+        
+      ticket.each_char do |c|
+        if c.upcase == "F"
+          row = Range.new(row.first, row.last - row.size / 2)
+        elsif c.upcase == "B"
+          row = Range.new(row.first + row.size / 2, row.last)
+        elsif c.upcase == "L"
+          seat = Range.new(seat.first, seat.last - seat.size / 2)
+        elsif c.upcase == "R"
+          seat = Range.new(seat.first + seat.size / 2, seat.last)
+        else
+          raise RuntimeError, "invalid input_data #{ticket}:#{c}"
+        end
+      end
+      (row.last * 8) + seat.last
     end
   end
 end
