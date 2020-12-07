37053a469e4cab9087b68f5df8dbbc138e447cd8
Tyler Fisher <tyler.fisher@amount.com>
2020-12-07 10:23:36 -0600

    Add #input_data for day_seven

--- lib/day_seven.rb
+++ lib/day_seven.rb
@@ -2,7 +2,23 @@ class DaySeven
   attr_accessor :input_data
 
   class << self
-    def input_data; end
+    def input_data
+      @input_data ||= begin
+        f = File.read('inputs/day_seven.txt')
+        f.split("\n").each_with_object([]) do |rule, array|
+          rule_hash = {}
+          split_rule = rule.chop.split('contain').map(&:strip)
+          rule_hash[:holding_bag] = split_rule[0].strip.sub(/ b\w+.$/, '')
+          rule_hash[:held_bags] = split_rule[1].split(', ').map do |held_bag|
+            {
+              quantity: held_bag.slice!(/^\d+/),
+              type: held_bag.strip.sub!(/ b\w+.$/, '')
+            }
+          end
+          array << rule_hash
+        end
+      end
+    end
 
     def part_one; end
 
